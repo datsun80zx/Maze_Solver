166c0e88af7b3682f912d5a27abd1cee13b9c66b
datsun80zx <m.richardp93@gmail.com>
2025-02-12 07:11:53 -0500

    added Cell class and refactored a bit

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from window_class import Window, Point, Line
+from window_class import Window, Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
-    pt1 = Point(2,3)
-    pt2 = Point(360,600)
-    line1 = Line(pt1, pt2)
+    x1 = 250
+    x2 = 500
+    y1 = 250
+    y2 = 500
+    
+    
+    cell1 = Cell(win)
+    cell1.right = False
+    cell1.draw(x1, x2, y1, y2)
 
-    win.draw_line(line1, "black")
+    # win.draw_line(line1, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- src/window_class.py
+++ src/window_class.py
@@ -12,16 +12,19 @@ class Line:
     
     def draw(self, canvas: Canvas, fill_color):
         canvas.create_line(
-            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+            self.p1.x, self.p1.y, 
+            self.p2.x, self.p2.y, 
+            fill=fill_color, 
+            width=2
         )
 
 class Window: 
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Window")
-        self.canvas = Canvas(self.__root, width=width, height=height)
-        self.canvas.pack()
-        self.running = False
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
@@ -29,16 +32,41 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running: 
+        self.__running = True
+        while self.__running: 
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
 
-    def draw_line(self, line: Line, fill_color):
-        line.draw(self.canvas, fill_color)
+    def draw_line(self, line: Line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
         
 
+class Cell:
+    def __init__(self, win: Window,):
+        self.left = True
+        self.right = True
+        self.top = True
+        self.bottom = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, x2, y1, y2):
+        if self.left:
+            line = Line(Point(x1,y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.top:
+            line = Line(Point(x1,y2), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.right:
+            line = Line(Point(x2,y2), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.bottom:
+            line = Line(Point(x2,y1), Point(x1, y1))
+            self._win.draw_line(line)
 
 
